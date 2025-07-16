e5006b532f748ac6a163a7b40ae75d6c9311221b
Naoto_ntp <sonimario101@gmail.com>
2025-07-15 20:24:01 -0400

    Update Variables.asm

--- Engine/Variables.asm
+++ Engine/Variables.asm
@@ -6,7 +6,7 @@
 NLZ_CONFIG	equ	4					; Set this value to one of the following values to configure the decompressor for your desired module size.
 								; 1 = $200 byte modules; 2 = $400 byte modules; 3 = $800 byte modules; 4 = $1000 byte modules; 5 = $2000 byte modules.
 NLZ_BUFFER_SIZE	equ	$100<<NLZ_CONFIG			; Size of the decompression buffer (in bytes).
-NLZ_QUEUE_SIZE	equ	16					; Number of slots in the decompression queue.
+NLZ_QUEUE_SIZE	equ	32					; Number of slots in the decompression queue.
 
 ; NLZ Queue Entry Offsets.
 nque:	struct	dots
